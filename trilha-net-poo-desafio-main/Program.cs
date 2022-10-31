6197526b494d957ac3cab7e5e614bf84719dacc6
Rinaldo845 <rinaldo700@hotmail.com>
2022-10-31 12:39:33 -0300

    Aprendendo mais de programação orientada a objetos

---
+++ trilha-net-poo-desafio-main/Program.cs
@@ -0,0 +1,18 @@
+﻿using DesafioPOO.Models;
+
+Console.WriteLine("Smartphone iphone:");
+Iphone iphone = new Iphone(numero: "1", modelo: "A", imei: "1",memoria: 9);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Spotify");
+
+Console.WriteLine("----------------------------------------------------------");
+
+Console.WriteLine("Smartphone Nokia:");
+Nokia nokia = new Nokia(numero: "2", modelo: "B", imei: "2",memoria: 15);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Telegram");
+
+Console.WriteLine("----------------------------------------------------------");
+// TODO: Realizar os testes com as classes Nokia e Iphone
